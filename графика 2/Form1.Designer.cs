014fc3059f81c16726f953708a78c0a1291f7352
Floyd Desert <gnomaxim73@gmail.com>
2017-05-26 00:51:55 +0300

    Добавьте файлы проекта.

---
+++ графика 2/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace графика_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(846, 460);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(397, 478);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 513);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ графика 2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+
+namespace графика_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            gr = pictureBox1.CreateGraphics();
+            gr.SmoothingMode = SmoothingMode.HighQuality;
+            snowflake = new Bitmap(200, 200);
+            Graphics snowflakeGraphics = Graphics.FromImage(snowflake);
+
+            snowflakeGraphics.DrawLine(blue, 0, 100, 200, 100);
+            snowflakeGraphics.DrawLine(blue, 150, 25, 50, 175);
+            snowflakeGraphics.DrawLine(blue, 50, 25, 150, 175);
+            
+            snowflakeGraphics.DrawLine(blue, 125, 55, 125, 20);
+            snowflakeGraphics.DrawLine(blue, 125, 55, 170, 45);
+
+            snowflakeGraphics.DrawLine(blue, 170, 100, 195, 80);
+            snowflakeGraphics.DrawLine(blue, 170, 100, 195, 120);
+            snowflakeGraphics.DrawLine(blue, 30, 100, 5, 80);
+            snowflakeGraphics.DrawLine(blue, 30, 100, 5, 120);
+
+            snowflakeGraphics.DrawLine(blue, 75, 145, 75, 180);
+            snowflakeGraphics.DrawLine(blue, 75, 145, 30, 155);
+
+            snowflakeGraphics.DrawLine(blue, 75, 55, 75, 20);
+            snowflakeGraphics.DrawLine(blue, 75, 55, 30, 45);
+            snowflakeGraphics.DrawLine(blue, 125, 145, 125, 180);
+            snowflakeGraphics.DrawLine(blue, 125, 145, 170, 155);
+        }
+        double t, step;
+        int startingX, xCoeff, yCoeff, size;
+        Random random = new Random();
+        Graphics gr;
+        Pen blue = new Pen(Color.FromArgb(50, 128, 128, 255), 3);
+        Bitmap snowflake;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t += step;
+            gr.DrawImage(snowflake, (float)(xCoeff*Math.Sin(t))+startingX, (float)t*yCoeff-size/2, size, size);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            t = 0;
+            step = random.NextDouble();
+            if (step < 0.01) step += 0.01;
+            while (step > 0.05) step -= 0.05;
+            xCoeff = random.Next(10, 100);
+            yCoeff = random.Next(10, 100);
+            size = random.Next(5, 200);
+            startingX = random.Next(0, pictureBox1.Width - 100);
+            timer1.Start();
+        }
+
+    }
+}
